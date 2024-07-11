a9615ebc472ba9c744a85487e850d84306c50677
sourav48765 <bisht4876@gmail.com>
2024-07-11 22:14:55 +0530

    redux react toolkit Todo

---
+++ reduxToolKitTodo/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AddTodo from './Components/AddTodo'
+import Todos from './Components/Todos'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Hello world</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
